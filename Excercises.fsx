c29b4e1c478eba8b58ad052dc70a203bae4b8e5f
raukodraug <chekempxo@gmail.com>
2018-04-17 10:05:59 -0600

    Started creating a few simple excercises.

---
+++ Excercises.fsx
@@ -0,0 +1,26 @@
+// ==============================
+// Excercises
+// ==============================
+
+(*
+Quick cheat-sheet
+
+Symbols
+* "=" is used instead of "=="
+* "<>" is used instead of "!="
+* "not" is used instead of "!"
+* In parameters, commas are replaced by whitespace
+* In non-parameter usage (eg lists), commas are replaced by semicolons in most places. 
+
+*)
+
+// 1. Create a "Hello world" function that introduces yourself. Where yourself is a parameter
+//    And run it
+
+
+
+// 2. Re-implement this function using partial application
+
+
+
+// 3. Use piping to pass the name into the function.
